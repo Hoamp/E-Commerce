d9201e3b93549c6bac89f4b805440dd6d2bc3f8c
Hoamp <setiawanthomas43@gmail.com>
2024-04-28 18:15:40 +0700

    middleware updated for customer,seller and admin

--- routes/web.php
+++ routes/web.php
@@ -42,26 +42,42 @@ Route::middleware(['auth'])->group(function(){
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 
+    // seller
+    Route::middleware(['is_seller'])->group(function(){
+
+    });
+    // end seller
+
+    // customer
+    Route::middleware(['is_customer'])->group(function(){
+
+    });
+    // end customer
+
     // admin
-    // admin produk
-    Route::get('/admin/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
+    Route::middleware(['is_admin'])->group(function(){
+        Route::get('/admin/dashboard', function () {
+            return view('admin.dashboard');
+        })->name('admin.dashboard');
+        
+        // admin produk
+        Route::get('/admin/produk', [AdminProdukController::class, 'index'])->name('admin.produk.index');
+        Route::get('/admin/produk/tambah-produk', [AdminProdukController::class, 'hal_tambah_produk'])->name('admin.produk.create');
+        Route::post('/admin/produk', [AdminProdukController::class, 'tambah_produk'])->name('admin.produk.store');
+        Route::delete('/admin/produk/{id}', [AdminProdukController::class, 'hapus_produk'])->name('admin.produk.delete');
+        Route::get('/admin/produk/{id}', [AdminProdukController::class, 'detail_produk'])->name('admin.produk.detail');
+        Route::get('/admin/produk/{id}/edit', [AdminProdukController::class, 'edit_produk'])->name('admin.produk.edit');
+        Route::put('/admin/produk/{id}/edit', [AdminProdukController::class, 'update_produk'])->name('admin.produk.update');
 
-    Route::get('/admin/produk', [AdminProdukController::class, 'index'])->name('admin.produk.index');
-    Route::get('/admin/produk/tambah-produk', [AdminProdukController::class, 'hal_tambah_produk'])->name('admin.produk.create');
-    Route::post('/admin/produk', [AdminProdukController::class, 'tambah_produk'])->name('admin.produk.store');
-    Route::delete('/admin/produk/{id}', [AdminProdukController::class, 'hapus_produk'])->name('admin.produk.delete');
-    Route::get('/admin/produk/{id}', [AdminProdukController::class, 'detail_produk'])->name('admin.produk.detail');
-    Route::get('/admin/produk/{id}/edit', [AdminProdukController::class, 'edit_produk'])->name('admin.produk.edit');
-    Route::put('/admin/produk/{id}/edit', [AdminProdukController::class, 'update_produk'])->name('admin.produk.update');
+        // admin category
+        Route::get('/admin/category', [CategoryController::class, 'index'])->name('admin.category.index');
+        Route::get('/admin/category/tambah-kategori', [CategoryController::class, 'hal_tambah_category'])->name('admin.category.create');
+        Route::post('/admin/category', [CategoryController::class, 'tambah_category'])->name('admin.category.store');
+        Route::delete('/admin/category/{id}', [CategoryController::class, 'hapus_category'])->name('admin.category.delete');
+        Route::get('/admin/category/{id}/edit', [CategoryController::class, 'edit_category'])->name('admin.category.edit');
+        Route::put('/admin/category/{id}/edit', [CategoryController::class, 'update_category'])->name('admin.category.update');
+    });
 
-    Route::get('/admin/category', [CategoryController::class, 'index'])->name('admin.category.index');
-    Route::get('/admin/category/tambah-kategori', [CategoryController::class, 'hal_tambah_category'])->name('admin.category.create');
-    Route::post('/admin/category', [CategoryController::class, 'tambah_category'])->name('admin.category.store');
-    Route::delete('/admin/category/{id}', [CategoryController::class, 'hapus_category'])->name('admin.category.delete');
-    Route::get('/admin/category/{id}/edit', [CategoryController::class, 'edit_category'])->name('admin.category.edit');
-    Route::put('/admin/category/{id}/edit', [CategoryController::class, 'update_category'])->name('admin.category.update');
     // end admin
 
 });
